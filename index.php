01db95e6e1863808598fc4386d03ed605f21b4ee
Jeremy <chambres.jeremy@gmail.com>
2024-04-10 16:10:56 +0200

    ajout igdb

--- index.php
+++ index.php
@@ -8,34 +8,13 @@
 <body>
     <?php
     require 'vendor/autoload.php';
-    $test = "ta mère";
-        $query = '
-            query ($id: Int) { # Define which variables will be used in the query (id)
-            Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-                id
-                title {
-                romaji
-                english
-                native
-                }
-            }
-            }
-        ';
-        // Define our query variables and values that will be used in the query request
-        $variables = [
-            "id" => 15125
-        ];
-        // Make the HTTP Api request
-        $http = new GuzzleHttp\Client;
-        var_dump($http);
-        $response = $http->post('https://graphql.anilist.co', [
-            'json' => [
-                'query' => $query,
-                'variables' => $variables,
-            ]
-        ]);
-
-        var_dump($response);
+    $clientId = "yyd6hmzyo9j8svgvfuzah6opbtuonw";
+    $clientSecret = "bs4tips201iqsya29at9ma12r5s9sr";
     ?>
+
+    <script>
+        let clientId = "yyd6hmzyo9j8svgvfuzah6opbtuonw";
+        let clientSecret = "bs4tips201iqsya29at9ma12r5s9sr"
+    </script>
 </body>
 </html>
